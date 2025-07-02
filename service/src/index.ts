3cc5e621bd28cc4ca97749887d63654f244ff0d4
Omri Gazitt <ogazitt@gmail.com>
2025-07-02 15:39:34 -0700

    fixing deployed GCP issue

--- service/src/index.ts
+++ service/src/index.ts
@@ -23,7 +23,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }))
-app.options('*', cors())
 
 const authzOptions = getConfig()
 
